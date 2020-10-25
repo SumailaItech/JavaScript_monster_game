18f5d6cf1669652d8ae29143d9b0d8ac1fe37a78
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-25 03:18:21 +0200

    Added a game reset to the application for resetting the game

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -10,6 +10,11 @@ let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
+function reset(){
+    currentMonsterHealth = chosenMaxLife;
+    currentPlayerHealth = chosenMaxLife;
+    resetGame(chosenMaxLife);
+}
 function endRound(){
     const initialPlayerHealth = currentPlayerHealth;
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
@@ -25,10 +30,15 @@ function endRound(){
 
     if(currentMonsterHealth <= 0 && currentPlayerHealth > 0){
         alert("You have won");
+        reset();
     }else if(currentPlayerHealth <= 0 && currentMonsterHealth > 0){
         alert("You lost");   
     }else if(currentPlayerHealth <= 0 && currentMonsterHealth <= 0){
         alert("You had a draw");
+    }
+
+    if(currentMonsterHealth <= 0 || currentPlayerHealth <= 0){
+        reset();
     }
 }
 
